724c8b29626c430c64911ab490850cde7e9392fd
Arko <arkadiusz.kupracz@gmail.com>
2020-07-07 14:29:28 +0100

    Add workout planning activity.

--- app/src/main/java/com/example/workoutplanner/PlanActivity.java
+++ app/src/main/java/com/example/workoutplanner/PlanActivity.java
@@ -3,12 +3,29 @@ package com.example.workoutplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PlanActivity extends AppCompatActivity {
+
+    private EditText workoutNameET;
+    private Button saveExerciseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+
+        workoutNameET = findViewById(R.id.editTextWorkoutName);
+        saveExerciseButton = findViewById(R.id.saveButton);
+        saveExerciseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String workoutName = workoutNameET.getText().toString();
+                Toast.makeText(getApplicationContext(),"Workout " + workoutName + " saved.",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
