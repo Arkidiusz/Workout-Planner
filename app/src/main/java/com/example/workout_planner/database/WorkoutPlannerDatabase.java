d28aa92bbd27748e1e85ac1a80cc474bef5210fb
Arko <arkadiusz.kupracz@gmail.com>
2020-08-08 15:45:10 +0100

    fix: Change app name to Workout Planner

--- app/src/main/java/com/example/workout_planner/database/WorkoutPlannerDatabase.java
+++ app/src/main/java/com/example/workout_planner/database/WorkoutPlannerDatabase.java
@@ -19,7 +19,7 @@ import com.example.workout_planner.database.workout_plan.WorkoutPlanDao;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Exercise.class, WorkoutPlan.class, ExerciseLog.class}, version = 11,
+@Database(entities = {Exercise.class, WorkoutPlan.class, ExerciseLog.class}, version = 12,
         exportSchema = false)
 @TypeConverters({Exercise.class, WorkoutPlan.class, ExerciseLog.class})
 public abstract class WorkoutPlannerDatabase extends RoomDatabase {
